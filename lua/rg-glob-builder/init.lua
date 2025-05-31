b6c8009f89911ef9321f0d664c01b4c49687d30c
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-05-31 15:33:09 -0400

    add comments to the config options, update README, docs

--- lua/rg-glob-builder/init.lua
+++ lua/rg-glob-builder/init.lua
@@ -28,27 +28,33 @@ local opts_schema = {
   },
 }
 
---- @class RgPatternBuilderSetupOptsCustomFlags
---- @field extension? string
---- @field file? string
---- @field directory? string
---- @field case_sensitive? string
---- @field ignore_case? string
---- @field whole_word? string
---- @field partial_word? string
 
 --- @class RgPatternBuilderSetupOpts
---- @field pattern_delimeter? string
---- @field custom_flags? RgPatternBuilderSetupOptsCustomFlags
---- @field nil_unless_trailing_space? boolean
+--- @field pattern_delimeter? string The single-char string to act as the delimeter for the pattern to pass to rg. Defaults to "~"
+--- @field custom_flags? RgPatternBuilderSetupOptsCustomFlags Custom flags
+--- @field nil_unless_trailing_space? boolean Return `nil` unless the final character is a trailing space. When updating the flags, this option will maintain the current search results until the update is complete. Defaults to `false`
 
+--- @class RgPatternBuilderSetupOptsCustomFlags
+--- @field extension? string The flag to include or negate an extension to the glob pattern. Extensions are prefixed internally with "*.". Defaults to "-e"
+--- @field file? string The flag to include or negate a file to the glob pattern. Files are passed without modification to the glob. Defaults to "-f"
+--- @field directory? string The flag to include or negate a directory to the glob pattern. Extensions are updated internally with "**/[directory]/**". Defaults to "-d"
+--- @field case_sensitive? string The flag to search case sensitively, adds the `--case-sensitive` flag. Defaults to "-c"
+--- @field ignore_case? string The flag to search case insensitively, adds the `--case-ignore` flag. Defaults to "-nc"
+--- @field whole_word? string The flag to search case by whole word, adds the `--word-regexp` flag. Defaults to "-w"
+--- @field partial_word? string The flag to search case by partial word, removes the `--word-regexp` flag (searching by partial word is the default behavior in rg). Defaults to "-w"
 
 local setup_opts = nil
 
 --- @param opts RgPatternBuilderSetupOpts
 M.setup = function(opts)
   if not validate(opts_schema, opts) then
-    error(string.format("Expected opts of type: %s, received %s", vim.inspect(opts_schema), vim.inspect(opts)))
+    error(
+      string.format(
+        "Malformed opts! Expected to match schema: %s, received %s",
+        vim.inspect(opts_schema),
+        vim.inspect(opts)
+      )
+    )
   end
 
   setup_opts = opts
