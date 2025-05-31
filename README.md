3dc6ae1c4cfc06ef457ed0e1dce54e6d7de16b21
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-05-30 22:31:41 -0400

    add empty test cases

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ WIP
 ## TODO
 - [ ] Accept options for search delimeters
 - [ ] Accept options for user defined flags
+- [ ] Accept option for trailing space
 - [ ] Unit tests for `build`
 - [ ] Adapter for fzf-lua
 - [ ] Adapter for telescope
--- tests/test_unit.lua
+++ tests/test_unit.lua
@@ -2,8 +2,27 @@ local expect = MiniTest.expect
 -- local child = MiniTest.new_child_neovim()
 
 local T = MiniTest.new_set()
-T["placeholder"] = function()
-  expect.equality("string", type "hello")
-end
+T["build"] = MiniTest.new_set()
+T["build"]["case"] = MiniTest.new_set()
+T["build"]["case"]["should default to ignore case"] = function() end
+T["build"]["case"]["should override to case sensitive"] = function() end
+
+T["build"]["whole word"] = MiniTest.new_set()
+T["build"]["whole word"]["should default to not search by whole word"] = function() end
+T["build"]["whole word"]["should override to search by whole word"] = function() end
+
+T["build"]["file"] = MiniTest.new_set()
+T["build"]["file"]["should include files"] = function() end
+T["build"]["file"]["should exclude files"] = function() end
+
+T["build"]["dirs"] = MiniTest.new_set()
+T["build"]["dirs"]["should include dirs"] = function() end
+T["build"]["dirs"]["should exclude dirs"] = function() end
+
+T["build"]["extensions"] = MiniTest.new_set()
+T["build"]["extensions"]["should include extensions"] = function() end
+T["build"]["extensions"]["should exclude extensions"] = function() end
+
+T["kitchen sink"] = function() end
 
 return T
